92ef6b4b91f67107edfff2b580dd3878bf0fef82
Johnathonanon <jconry21@gmail.com>
2021-11-15 17:08:51 +0000

    Incorporate select class function into Player class, import randint, create build player function in Player class.

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import enquiries
 
+from random import randint
+
 
 class Player():
     """
@@ -13,19 +15,20 @@ class Player():
         self.hp = hp
         self.attack = attack
 
-    def print_name():
-        print(name)
+    @classmethod
+    def build_player(cls):
+        name = input("Please enter your name: ")
+        classes = ['Warrior', 'Wizard', 'Archer']
+        player_class = enquiries.choose('Please choose a class:\n', classes)
+
+        return cls(name, player_class)
 
 
 def start():
     """
-    Starts game,prompts player for name and confirms progression of game
+    Starts game and confirms user wishes to play
     """
     print("\nWelcome Adventurer!\n")
-    print("What is your name?\n")
-    global name
-    name = input("My name is: ")
-    print(f"\nWelcome {name}.")
 
     while True:
 
@@ -33,21 +36,13 @@ def start():
 
         if start_prompt == "y":
             print("\nExcellent, Lets go!\n")
-            select_class()
+            # create_player()
             return False
         elif start_prompt == "n":
             abort_game()
             return False
         else:
             print("\nPlease enter either 'y' or 'n'")
-
-
-def select_class():
-
-    classes = ['Warrior', 'Wizard', 'Archer']
-    choice = enquiries.choose('First please choose a class:\n', classes)
-
-    print(choice)
 
 
 def abort_game():
