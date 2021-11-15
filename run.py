dffb678becc2e64a365da8ab9894178d6f644c5a
Johnathonanon <jconry21@gmail.com>
2021-11-15 13:52:52 +0000

    Make minor amendments to start function. Import enquiries and write select_class function.

--- run.py
+++ run.py
@@ -1,27 +1,42 @@
+import enquiries
+
+
 def start():
     """
     Starts game and prompts player for name
     """
-    print("Welcome Adventurer!\n")
+    print("\nWelcome Adventurer!\n")
     print("What is your name?\n")
+    global name
     name = input("My name is: ")
-    print(f"\nWelcome {name}.\n")
-    start_prompt = input("Are you ready to begin? y/n ").lower()
+    print(f"\nWelcome {name}.")
 
-    if start_prompt == "y":
-        play_game()
-    elif start_prompt == "n":
-        abort_game()
-    else:
-        print("Please input either 'y' or 'n'")
+    while True:
+
+        start_prompt = input("\nAre you ready to begin? y/n ").lower()
+
+        if start_prompt == "y":
+            print("\nExcellent, Lets go!")
+            select_class()
+            return False
+        elif start_prompt == "n":
+            abort_game()
+            return False
+        else:
+            print("\nPlease enter either 'y' or 'n'")
 
 
-def play_game():
-    print("Letsa go!")
+def select_class():
+    print("\nFirst please choose a class.\n")
+
+    classes = ['Warrior', 'Wizard', 'Archer']
+    choice = enquiries.choose('Choose one of these options: ', classes)
+
+    print(choice)
 
 
 def abort_game():
-    print("k bye")
+    print("\nWell that's a pity :( Bye Bye")
 
 
 start()
