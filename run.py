08b58c0dddbe61d92bdbb731ca7f8aa5b01bef5b
Johnathonanon <jconry21@gmail.com>
2021-11-15 13:07:43 +0000

    Write start function and carry out some preliminary testing in terminal

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def start():
+    """
+    Starts game and prompts player for name
+    """
+    print("Welcome Adventurer!\n")
+    print("What is your name?\n")
+    name = input("My name is: ")
+    print(f"\nWelcome {name}.\n")
+    start_prompt = input("Are you ready to begin? y/n ").lower()
+
+    if start_prompt == "y":
+        play_game()
+    elif start_prompt == "n":
+        abort_game()
+    else:
+        print("Please input either 'y' or 'n'")
+
+
+def play_game():
+    print("Letsa go!")
+
+
+def abort_game():
+    print("k bye")
+
+
+start()
