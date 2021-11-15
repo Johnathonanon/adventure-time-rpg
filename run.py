0f4aeb9b614f3ab99c6447b77af80efe78c2de3e
Johnathonanon <jconry21@gmail.com>
2021-11-15 17:50:30 +0000

    Complete build_player method in Player class and add print statements

--- run.py
+++ run.py
@@ -9,19 +9,36 @@ class Player():
     Has methods for progressing through dungeon, exploring, interacting
     and attacking
     """
-    def __init__(self, name, player_class, hp, attack):
+    def __init__(self, name, player_class, health_points, attack):
         self.name = name
         self.player_class = player_class
-        self.hp = hp
+        self.health_points = health_points
         self.attack = attack
 
     @classmethod
     def build_player(cls):
+
         name = input("Please enter your name: ")
         classes = ['Warrior', 'Wizard', 'Archer']
-        player_class = enquiries.choose('Please choose a class:\n', classes)
+        player_class = enquiries.choose('\nPlease choose a class:\n', classes)
 
-        return cls(name, player_class)
+        if player_class == 'Warrior':
+            health_points = randint(40, 50)
+            attack = randint(5, 10)
+        elif player_class == 'Wizard':
+            health_points = randint(20, 30)
+            attack = randint(15, 20)
+        else:
+            health_points = randint(30, 40)
+            attack = randint(10, 15)
+
+        print(
+            f"Welcome {name}!\n"
+            f"You are playing as a {player_class}\n"
+            f"Your HP is {health_points}\n"
+            f"You have an attack of {attack}")
+
+        return cls(name, player_class, health_points, attack)
 
 
 def start():
@@ -36,7 +53,7 @@ def start():
 
         if start_prompt == "y":
             print("\nExcellent, Lets go!\n")
-            # create_player()
+            Player.build_player()
             return False
         elif start_prompt == "n":
             abort_game()
